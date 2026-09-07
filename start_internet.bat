71a42f0fd095d22a889dd6d52a66bc5a23497525
Денис <131889924+Gupkagrop@users.noreply.github.com>
2026-09-07 15:38:01 +0500

    Change tunnel to Pinggy in start_internet.bat

--- start_internet.bat
+++ start_internet.bat
@@ -7,11 +7,9 @@ echo Запуск Frontend-сервера...
 start cmd /k "cd frontend && npm run dev -- --host"
 
 echo =======================================================
-echo Запуск туннеля в интернет (Localtunnel)...
-echo Пожалуйста, подождите пару секунд, пока сгенерируется публичная ссылка!
-echo Скопируйте ссылку, которая появится ниже (начинается с https://).
-echo.
-echo ВАЖНО: При переходе с телефона вас может попросить
-echo нажать синюю кнопку "Click to Continue", чтобы подтвердить вход.
+echo Запуск туннеля в интернет (Pinggy)...
+echo На экране появится QR-код и HTTPS-ссылка.
+echo Если консоль спросит "Are you sure you want to continue connecting (yes/no)?",
+echo введите yes и нажмите Enter.
 echo =======================================================
-cmd /k "npx localtunnel --port 5173 --local-https --allow-invalid-cert"
+cmd /k "ssh -p 443 -R0:localhost:5173 a.pinggy.io"
